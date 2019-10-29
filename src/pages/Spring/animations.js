85016f6c094e71ed7c736c80a8a2303de787cfd7
Luciano de Souza Nery <lsnery@gmail.com>
2019-10-29 19:56:08 -0300

    added debug function

--- src/pages/Spring/animations.js
+++ src/pages/Spring/animations.js
@@ -4,6 +4,10 @@ const state = {
 	animation: new Animated.Value(1),
 };
 
+const debug = () => {
+	state.animation.addListener(({ value }) => console.log(value));
+};
+
 const timing = (value, during) => {
 	return Animated.timing(state.animation, {
 		toValue: value,
@@ -20,6 +24,7 @@ const spring = (toValue, friction, tension) => {
 };
 
 const startAnimation = () => {
+	debug();
 	spring(2, 2, 160).start(() => timing(1, 100).start());
 };
 
